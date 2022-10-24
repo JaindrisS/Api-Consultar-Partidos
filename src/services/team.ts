84f38c45b58931c984062750a87abc685a237fdd
Jaindris Sosa <jaindrissosajsd@gmail.com>
2022-10-24 17:32:43 -0500

    Add service to create new game

--- src/services/team.ts
+++ src/services/team.ts
@@ -1,4 +1,4 @@
-import { Team } from "../interfaces/team";
+import { Games, Team } from "../interfaces/team";
 import { TeamModel } from "../models/team";
 
 export const createTeamService = async (team: Team) => {
@@ -9,6 +9,24 @@ export const createTeamService = async (team: Team) => {
 
 export const getTeamService = async () => {
   const response = await TeamModel.find({ status: true });
+
+  return response;
+};
+
+export const addGameService = async (id: string, games: Games) => {
+  const { rival, goalsConceded, goalsScored, date, points } = games;
+
+  const response = await TeamModel.findByIdAndUpdate(id, {
+    $push: {
+      games: {
+        rival,
+        goalsConceded,
+        goalsScored,
+        date,
+        points,
+      },
+    },
+  });
 
   return response;
 };
