b81764e8422d5ffe1b7be3e88fc060c6bff5c881
Jaindris Sosa <jaindrissosajsd@gmail.com>
2022-10-27 10:58:41 -0500

    Add controller service to get the latest game

--- src/services/team.ts
+++ src/services/team.ts
@@ -30,3 +30,30 @@ export const addGameService = async (id: string, games: Games) => {
 
   return response;
 };
+
+export const getLastGameService = async (id: string) => {
+  //    I get the information of the equipment by its id,
+  const infoTeam = await TeamModel.findById(id);
+
+  // traverse and save the dates of the array
+  const dateTeam = infoTeam?.games.map((e) => {
+    return e.date;
+  });
+
+  //check major date
+  const date: any = dateTeam?.map((e) => new Date(e));
+  const dateMax = new Date(Math.max.apply(null, date));
+
+  // Transform to string
+  const dateString = dateMax.toISOString();
+
+  // get only the date
+  const onlyDate = dateString.substring(0, 10);
+
+  // return the match that is equal to the given date
+  const response = infoTeam?.games.filter((e) => {
+    return e.date === onlyDate;
+  });
+
+  return response;
+};
