9e5036540bcdf0314fe5e8cdaead60422dbd4f87
Jaindris Sosa <jaindrissosajsd@gmail.com>
2022-11-17 19:57:24 -0500

    Add user schema

---
+++ src/models/user.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+import { User } from "../interfaces/user";
+
+const userSchema = new Schema<User>(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+    },
+
+    status: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const UserModel = model("User", userSchema);
