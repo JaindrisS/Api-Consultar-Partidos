9a74dd8ecdf2a3f2138fd2a82ce201ba1ed6e263
Jaindris Sosa <jaindrissosajsd@gmail.com>
2022-11-18 20:21:26 -0500

    Add controller service, for login

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,6 @@
-import { User } from "../interfaces/user";
+import { User, LogIn } from "../interfaces/user";
 import { UserModel } from "../models/user";
-import { hash } from "../utils/bcript";
+import { comparePassword, hash } from "../utils/bcript";
 
 export const signUpService = async (data: User) => {
   const { password, email, name } = data;
@@ -17,4 +17,23 @@ export const signUpService = async (data: User) => {
 
   await response.save();
   return response;
+};
+
+export const logInService = async (data: LogIn) => {
+  const { password, email } = data;
+
+  const user: any = await UserModel.findOne({
+    email: { $regex: email, $options: "i" },
+  });
+
+  if (!user || !user.status) {
+    return { msg: "Invalid email or password" };
+  }
+
+  const userValid = await comparePassword(password, user.password);
+  if (!userValid) {
+    return { msg: "Invalid email or password" };
+  }
+
+  return "Login Ok";
 };
