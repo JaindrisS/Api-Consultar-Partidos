0cf4c980c0048882e4d7d41e644781f22ee8cf92
Jaindris Sosa <jaindrissosajsd@gmail.com>
2022-11-18 20:19:13 -0500

    add router and validations,for login

--- src/v1/routers/auth.ts
+++ src/v1/routers/auth.ts
@@ -1,24 +1,33 @@
 import { Router } from "express";
-import { signUp } from "../../controllers/auth";
+import { logIn, signUp } from "../../controllers/auth";
 import { body, param } from "express-validator";
 import { validateField } from "../../middlewares/validateresult";
 import { emailAlreadyExists } from "../../utils/dbValidations";
 
 const app = Router();
 
-app.post(
-  "/sign-up",
-  [
-    body("password", "Enter a valid password minimum 6 values maximum 14 ")
-      .notEmpty()
-      .isLength({ min: 6, max: 14 }),
-    body("email", "Enter a valid email")
-      .notEmpty()
-      .isEmail()
-      .custom(emailAlreadyExists),
-    validateField,
-  ],
-  signUp
-);
+app
+  .post(
+    "/sign-up",
+    [
+      body("password", "Enter a valid password minimum 6 values maximum 14 ")
+        .notEmpty()
+        .isLength({ min: 6, max: 14 }),
+      body("email", "Enter a valid email")
+        .notEmpty()
+        .isEmail()
+        .custom(emailAlreadyExists),
+      validateField,
+    ],
+    signUp
+  )
+  .post(
+    "/log-in",
+    [
+      body(["password", "email"], "Enter an email and a password").notEmpty(),
+      validateField,
+    ],
+    logIn
+  );
 
 export default app;
